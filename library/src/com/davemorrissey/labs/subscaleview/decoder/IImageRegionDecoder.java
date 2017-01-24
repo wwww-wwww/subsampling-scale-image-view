44aa4425c8c9df8998c67e8abbcd8d229bfd341c
len <len@kanade.eu>
2017-01-24 20:20:17 +0100

    Update Image region decoder

--- library/src/com/davemorrissey/labs/subscaleview/decoder/IImageRegionDecoder.java
+++ library/src/com/davemorrissey/labs/subscaleview/decoder/IImageRegionDecoder.java
@@ -1,6 +1,5 @@
 package com.davemorrissey.labs.subscaleview.decoder;
 
-import android.content.ContentResolver;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Point;
@@ -9,7 +8,6 @@ import android.net.Uri;
 
 import com.hippo.image.BitmapDecoder;
 import com.hippo.image.BitmapRegionDecoder;
-import com.hippo.image.ImageInfo;
 
 import java.io.InputStream;
 
@@ -19,24 +17,16 @@ public class IImageRegionDecoder implements ImageRegionDecoder {
 
     @Override
     public Point init(Context context, Uri uri) throws Exception {
-        ImageInfo info = new ImageInfo();
-        ContentResolver resolver = context.getContentResolver();
-
-        InputStream is = null;
+        InputStream inputStream = null;
         try {
-            is = resolver.openInputStream(uri);
-            boolean result = BitmapDecoder.decode(is, info);
-            if (!result) {
-                throw new RuntimeException("Image info returned false");
-            }
+            inputStream = context.getContentResolver().openInputStream(uri);
+            decoder = BitmapRegionDecoder.newInstance(inputStream);
         } finally {
-            if (is != null) {
-                try { is.close(); } catch (Exception e) { /* Empty */ }
+            if (inputStream != null) {
+                try { inputStream.close(); } catch (Exception e) { }
             }
         }
-
-        decoder = BitmapRegionDecoder.newInstance(resolver.openInputStream(uri));
-        return new Point(info.width, info.height);
+        return new Point(decoder.getWidth(), decoder.getHeight());
     }
 
     @Override
